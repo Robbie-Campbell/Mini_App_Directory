368a21a30a5a6beb93e3fd1790ceb1e189f30189
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-08-27 17:03:12 +0100

    :bento: small syntax changes :bento:

--- Classes/Home/choice.py
+++ Classes/Home/choice.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter.font import Font
 from Classes.Cards import card_gui
 from Classes.View import view_gui
+import os
 
 
 # This is a class which extends to all of the children classes
@@ -13,31 +14,38 @@ class Options(tk.Frame):
         self.master.size()
         self.grid()
 
+        self.root = os.getenv("RootPathPython")
+        # Create a home image
+        self.canvas = tk.Canvas(self, width=200, height=200, bd=0, highlightthickness=0, bg="#000")
+        self.img = tk.PhotoImage(file=self.root + "/Mini_App_Directory/Classes/Home/python.png")
+        self.canvas.create_image(0, 0, anchor="nw", image=self.img)
+        self.canvas.grid(row=0, column=0, rowspan=3)
+
         self.arial16 = Font(family="arial", size=16, weight="bold")
         self.title = tk.Label(self, font=self.arial16, bg="#FFF")
         self.title["text"] = "SELECT AN OPTION"
-        self.title.grid(row=0, sticky="news", ipadx=100, ipady=20)
+        self.title.grid(row=0, column=1, sticky="news", ipadx=40, ipady=20)
 
         # The list of buttons which create the new classes
         self.card_option = tk.Button(self, font=self.arial16, bg="#225", fg="#DDD")
         self.card_option["text"] = "RANDOM CARD GENERATOR"
         self.card_option["command"] = self.open_card_game
-        self.card_option.grid(row=1, sticky="news", ipady=10)
+        self.card_option.grid(row=1, column=1, sticky="news", ipady=10)
 
         self.view_option = tk.Button(self, font=self.arial16, bg="#522", fg="#DDD")
         self.view_option["text"] = "IMAGE GALLERY"
         self.view_option["command"] = self.image_gallery
-        self.view_option.grid(row=2, sticky="news", ipady=10)
+        self.view_option.grid(row=2, column=1, sticky="news", ipady=10)
 
     # Open the card guessing game
     def open_card_game(self):
         self.destroy()
-        card_game = card_gui.Application(self.master)
+        card_gui.Application(self.master)
         self.master.title("RANDOM CARD GENERATOR")
 
     def image_gallery(self):
         self.destroy()
-        beautiful = view_gui.TheView(self.master)
+        view_gui.TheView(self.master)
         self.master.title("Image Gallery")
 
 
